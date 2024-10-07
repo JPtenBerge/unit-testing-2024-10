d33e1c297f5aa3ee38378421128f986c9c54cd57
JP <jp@jp.nl>
2024-10-07 10:39:38 +0200

    Eerste tests met MSTest

---
+++ DemoSolution/DemoProject.Tests/UnitTest1.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace DemoProject.Tests;
+
+[TestClass]
+public class CalculatorTests
+{
+    Calculator _sut;
+
+    [TestInitialize] // async inheritance - gaat af vooraf aan IEDERE test
+    public void Init()
+    {
+        _sut = new Calculator();
+    }
+
+    //public CalculatorTests()
+    //{
+           
+    //}
+    //~CalculatorTests()
+    //{
+
+    //}
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(1, 1);
+        Assert.AreEqual("hoi", "doei");
+    }
+
+    // CalculatorAddShouldIncrementWhen()
+    // WhenDitThen
+    // GivenWhenThen
+    // UnitOfWork_State_ExpectedBehavior
+    // 
+
+
+    [TestMethod]
+    public void Add_PositiveIntegers_AddsNumbers()
+    {
+        // Arrange - setup
+        
+        _sut.Add(4);
+
+        // Act - doen
+        _sut.Add(8);
+
+        // Assert - checken
+        Assert.AreEqual(12, _sut.Result);
+        Assert.AreEqual(12, _sut.Result);
+        Assert.AreEqual(12, _sut.Result);
+        Assert.AreEqual(12, _sut.Result);
+        Assert.AreEqual(12, _sut.Result);
+    }
+
+    [TestMethod]
+    public void Add_MixedPositiveAndNegativeIntegers_AddsNumbers()
+    {
+        // system under test
+        _sut.Add(-4);
+        _sut.Add(8);
+        _sut.Add(-15);
+        Assert.AreEqual(-11, _sut.Result);
+    }
+
+    [TestMethod]
+    public void AddTest3()
+    {
+        // system under test
+        _sut.Minus(4);
+        _sut.Minus(8);
+
+        Assert.AreEqual(-12, _sut.Result);
+    }
+
+    [TestMethod]
+    public void AddTest4()
+    {
+        //Assembly.GetExecutingAssembly().GetTypes().First().GetMethods(BindingFlags.).First().Invoke()
+
+        // system under test
+        _sut.Result = -3;
+        _sut.Minus(9);
+
+        Assert.AreEqual(-12, _sut.Result);
+    }
+}
---
+++ DemoSolution/DemoProject/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject;
+
+//[InternalsVisibleTo("DemoProject.Tests")]
+public class Calculator
+{
+    public int Result { get; set; }
+
+    public void Add(int x)
+    {
+        Result += x;
+    }
+
+    public void Minus(int x)
+    {
+        Result -= x;
+    }
+}
