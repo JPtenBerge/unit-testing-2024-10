751d1268c14a986cb4b3640a0439e4b05ddd2f2c
JP <jp@jp.nl>
2024-10-08 14:07:19 +0200

    Integration testing met testcontainers

---
+++ DemoSolution/DemoProject.Tests/DatabaseWebcontainer.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+
+namespace DemoProject.Tests;
+
+[TestClass]
+public class DatabaseWebcontainer
+{
+    // this is called for each test, since each test
+    // instantiates a new class instance
+    private readonly PostgreSqlContainer container =
+        new PostgreSqlBuilder()
+        .Build();
+    private string connectionString = string.Empty;
+
+    [TestMethod]
+    public async Task Database_Can_Run_Query()
+    {
+        await container.StartAsync();
+        connectionString = container.GetConnectionString();
+
+        await using NpgsqlConnection connection = new(connectionString);
+        await connection.OpenAsync();
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT '1';";
+        var result = command.ExecuteScalar();
+        
+
+        //const int expected = 1;
+        //var actual = await connection..QueryFirstAsync<int>("SELECT 1");
+        Assert.AreEqual("1", result);
+    }
+}
---
+++ DemoSolution/DemoProject.Tests/HelloWorldWebcontainer.cs
@@ -0,0 +1,45 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.Tests
+{
+    [TestClass]
+    public class HelloWorldWebcontainer
+    {
+        static IContainer _container = new ContainerBuilder()
+            // Set the image for the container to "testcontainers/helloworld:1.1.0".
+            .WithImage("testcontainers/helloworld:1.1.0")
+            // Bind port 8080 of the container to a random port on the host.
+            .WithPortBinding(8080, true)
+            // Wait until the HTTP endpoint of the container is available.
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8080)))
+            // Build the container configuration.
+            .Build();
+
+        [ClassInitialize]
+        public static async Task InitAll(TestContext ctx)
+        {
+            await _container.StartAsync();
+        }
+
+        [TestMethod]
+        public async Task MyFirstTestcontainer()
+        {
+            var httpClient = new HttpClient();
+            var requestUri =
+                new UriBuilder(
+                    Uri.UriSchemeHttp,
+                    _container.Hostname,
+                    _container.GetMappedPublicPort(8080)
+                ).Uri;
+            var html = await httpClient.GetStringAsync(requestUri);
+            Assert.AreEqual("hoi", html);
+        }
+    }
+}
